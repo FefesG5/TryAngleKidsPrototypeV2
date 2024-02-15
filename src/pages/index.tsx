0821b28f386ed27162dfd2abcf7f99adfbd8acd4
Gee Chai <fc00004@gmail.com>
2024-02-15 12:30:57 +0900

    feat: basic setup navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,26 @@
+import Link from "next/link";
+import styles from "./Home.module.css";
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>
+        Welcome to Try Angle Kids Video Lessons
+      </h1>
+      <nav>
+        <ul className={styles.navList}>
+          <li>
+            <Link href="/lesson1" className={styles.link}>
+              Lesson 1
+            </Link>
+          </li>
+          <li>
+            <Link href="/lesson2" className={styles.link}>
+              Lesson 2
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
