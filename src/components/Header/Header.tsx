e7adcd11e59eb82a9056f21476aebc8167787c4d
Gee Chai <fc00004@gmail.com>
2024-02-16 14:13:03 +0900

    feat: Try Angle Kids logo replace Next logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,11 +8,11 @@ const Header: React.FC = () => {
     <header className={styles.header}>
       <div>
         <Image
-          src="/next.svg" // Assuming you have a Next.js logo SVG in your public directory
-          alt="Next.js Logo"
+          src="/logo.svg" // Assuming you have a Next.js logo SVG in your public directory
+          alt="Try Angle Kids Logo"
           className={styles.logo}
-          width={128} // Adjust to the size of your actual logo
-          height={64} // Adjust to the size of your actual logo
+          width={300}
+          height={100}
         />
       </div>
       {/* Add navigation items or other content here */}
