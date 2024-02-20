bd26bc5a3029868505c2f6692d699447006cdb5d
Gee Chai <fc00004@gmail.com>
2024-02-20 12:31:55 +0900

    feat: Try Angle Kids logo links to homepage

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,18 +2,21 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./Header.module.css";
+import Link from "next/link";
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <div>
-        <Image
-          src="/logo.svg" // Assuming you have a Next.js logo SVG in your public directory
-          alt="Try Angle Kids Logo"
-          className={styles.logo}
-          width={300}
-          height={100}
-        />
+        <Link href="/">
+          <Image
+            src="/logo.svg" // Assuming you have a Next.js logo SVG in your public directory
+            alt="Try Angle Kids Logo"
+            className={styles.logo}
+            width={300}
+            height={100}
+          />
+        </Link>
       </div>
       {/* Add navigation items or other content here */}
     </header>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,9 @@ import styles from "./Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>
-      Try Angle Kids Interactive Quiz
-      </h1>
+      <h1 className={styles.heading}>Try Angle Kids Interactive Quiz</h1>
       <Link href="/lessons" passHref>
-        <button className={styles.callToAction}>
-          Start Learning
-        </button>
+        <button className={styles.callToAction}>Start Learning</button>
       </Link>
     </div>
   );
