65b14174cbb9af6f63db8bf22fe1039b731a6b5e
Gee Chai <fc00004@gmail.com>
2024-02-20 12:50:34 +0900

    feat: report-issue.tsx page added

---
+++ src/pages/report-issue.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ReportIssue = () => {
+  return (
+    <>
+      <h1>Report an Issue</h1>
+      <p>If you encounter any problems, please let us know.</p>
+      {/* Here you can add a form or instructions for users to report issues */}
+    </>
+  );
+};
+
+export default ReportIssue;
