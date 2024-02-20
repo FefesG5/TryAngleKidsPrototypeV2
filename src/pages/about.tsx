89cb065b0210adfede3d131cee52309ee8ceea69
Gee Chai <fc00004@gmail.com>
2024-02-20 12:49:07 +0900

    feat: about page added

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h1>About Try Angle Kids</h1>
+      <p>
+        This is the about page where you can learn more about Try Angle Kids.
+      </p>
+    </>
+  );
+};
+
+export default About;
