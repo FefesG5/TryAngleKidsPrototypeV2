4675cf226de6c943b8c8e3ffd7a5c9aa1e222ae0
Gee Chai <fc00004@gmail.com>
2024-02-26 09:48:59 +0900

    chore: videoLessonData added

---
+++ src/data/videoLessonData.ts
@@ -0,0 +1,44 @@
+export const videoLessonData = {
+  videoId: "2wUsdsae0ro",
+  videoSrc: "https://www.youtube.com/embed/2wUsdsae0ro",
+  category: "measurements",
+  questions: [
+    {
+      question: "What is the purpose of measuring?",
+      timestamp: 70,
+      choices: [
+        "To create confusion",
+        "To find out the size or the amount of something, like time, weight, temperature, or speed",
+        "To pass the time",
+        "To make things complicated",
+      ],
+      correctAnswer:
+        "To find out the size or the amount of something, like time, weight, temperature, or speed",
+    },
+    {
+      question:
+        "What are some examples of standard units for measuring length?",
+      timestamp: 101,
+      choices: [
+        "Inches and centimeters",
+        "Salamanders and turtles",
+        "Cows and hats",
+        "Paper clips and erasers",
+      ],
+      correctAnswer: "Inches and centimeters",
+    },
+    {
+      question:
+        "What are the important rules mentioned for measuring length with non-standard units?",
+      timestamp: 201,
+      choices: [
+        "All units must be different sizes",
+        "Units should overlap each other",
+        "Units should be the same size and direction, should touch but not overlap, usually make a straight line, and measurement starts at one end and finishes at the other end",
+        "Start measuring from the center of the object",
+      ],
+      correctAnswer:
+        "Units should be the same size and direction, should touch but not overlap, usually make a straight line, and measurement starts at one end and finishes at the other end",
+    },
+  ],
+};
