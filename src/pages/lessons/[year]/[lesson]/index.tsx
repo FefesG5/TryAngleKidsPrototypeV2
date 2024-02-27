0f4dabd55d71371552f5da096d8a0e4c01420699
Gee Chai <fc00004@gmail.com>
2024-02-27 14:30:42 +0900

    refactor: remove all video and quiz component item, rework the quiz data

--- src/pages/lessons/[year]/[lesson]/index.tsx
+++ src/pages/lessons/[year]/[lesson]/index.tsx
@@ -1,21 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { useRouter } from "next/router";
-import VideoPlayer from "@/components/VideoPlayer/VideoPlayer"; // Adjust the path as necessary
-import { videoLessonData } from "@/data/videoLessonData"; // Adjust the path as necessary
-import Quiz from "@/components/Quiz/Quiz";
 
 const LessonDetails = () => {
   const router = useRouter();
   const { year, lesson } = router.query;
 
-  const [currentVideoTime, setCurrentVideoTime] = useState(0); // State to track video time
 
-  const showVideoPlayer =
-    year === videoLessonData.year && lesson === videoLessonData.lessonNumber;
-
-  const handleVideoTimeUpdate = (time: number) => {
-    setCurrentVideoTime(time);
-  };
 
   return (
     <div>
@@ -23,18 +13,7 @@ const LessonDetails = () => {
       <p>
         Details for lesson {lesson} in the year {year}.
       </p>
-      {showVideoPlayer && (
-        <>
-          <VideoPlayer
-            videoSrc={videoLessonData.videoSrc}
-            onTimeUpdate={handleVideoTimeUpdate} // Pass the callback function
-          />
-          <Quiz
-            currentTime={currentVideoTime} // Pass the current time to the quiz
-            questions={videoLessonData.questions}
-          />
-        </>
-      )}
+  
     </div>
   );
 };
