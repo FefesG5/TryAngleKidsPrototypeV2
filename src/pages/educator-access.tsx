50784f20e87338f396d8af35399b1b2e26f3d9e4
Gee Chai <fc00004@gmail.com>
2024-03-03 17:03:46 +0900

    feat: google auth check for educator on whitelist

--- src/pages/educator-access.tsx
+++ src/pages/educator-access.tsx
@@ -1,55 +1,81 @@
-import { app } from "../../firebaseConfig";
+import { useEffect, useState } from "react";
 import {
   getAuth,
   signInWithPopup,
   GoogleAuthProvider,
   signOut,
 } from "firebase/auth";
-import { useEffect, useState } from "react";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import { app } from "../../firebaseConfig";
 
 const auth = getAuth(app);
+const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
 
 const EducatorAccess = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setIsAuthenticated(!!user);
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        // Check if user is in the 'educators' list
+        const educatorsQuery = query(
+          collection(db, "educators"),
+          where("email", "==", user.email),
+        );
+        const querySnapshot = await getDocs(educatorsQuery);
+        if (!querySnapshot.empty) {
+          setIsAuthenticated(true);
+          setError("");
+        } else {
+          setError("You are not authorised. Please contact Head of Curriculum");
+          await signOut(auth);
+        }
+      } else {
+        setIsAuthenticated(false);
+      }
     });
+
     return () => unsubscribe();
   }, []);
 
-  const handleGoogleSignIn = async () => {
+  const signInWithGoogle = async () => {
     try {
       await signInWithPopup(auth, googleProvider);
-      console.log("Sign in successful");
-    } catch (e) {
-      console.error("Sign in Error", e);
+    } catch (error) {
+      setError("Failed to sign in. Please try again.");
     }
   };
 
-  const handleSignOut = async () => {
+  const signOutUser = async () => {
     try {
       await signOut(auth);
-      console.log("Signed out successful");
-    } catch (e) {
-      console.error("Sign out error:", e);
+      setIsAuthenticated(false); // Ensure the user is set as not authenticated
+    } catch (error) {
+      setError("Error signing out. Please try again.");
     }
   };
 
   return (
     <>
       <h1>Educator Access</h1>
+      {error && <p>{error}</p>}
       {isAuthenticated ? (
         <>
           <p>You are logged in.</p>
-          <button onClick={handleSignOut}>Sign Out</button>
+          <button onClick={signOutUser}>Sign Out</button>
         </>
       ) : (
         <>
           <p>Please sign in to access educator features.</p>
-          <button onClick={handleGoogleSignIn}>Sign in with Google</button>
+          <button onClick={signInWithGoogle}>Sign in with Google</button>
         </>
       )}
     </>
