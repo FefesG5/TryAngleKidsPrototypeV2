ae74034ffc451d974a2d88bbc746ddcc301ff92e
Gee Chai <fc00004@gmail.com>
2024-03-05 15:06:50 +0900

    feat: ThemeContext Provider added

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,33 @@
+import { useState, createContext, ReactNode } from "react";
+
+// Define the shape of the context data
+interface ThemeContextType {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+// Create the context with a default value and type
+const ThemeContext = createContext<ThemeContextType>({
+  theme: "light",
+  toggleTheme: () => {},
+});
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export { ThemeContext, ThemeProvider };
