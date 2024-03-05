bf788c77b8b5b715f6318343ae77b9a65d5602cf
Gee Chai <fc00004@gmail.com>
2024-03-05 09:56:58 +0900

    feat: nprogress added to display page switching loading time

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,18 @@ import type { AppProps } from "next/app";
 import RootLayout from "@/app/RootLayout";
 import "@/styles/globals.css";
 
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+import { Router } from "next/router";
+
+// Setup NProgress for each route change
+NProgress.configure({ showSpinner: false });
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RootLayout>
