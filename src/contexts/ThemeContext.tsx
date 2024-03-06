582cb5ad3e1445166c5ea14804f187001aac45e8
Gee Chai <fc00004@gmail.com>
2024-03-06 10:47:16 +0900

    fix: background dark theme image switches with light theme

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import { useState, createContext, ReactNode, useEffect } from "react";
+import { useState, createContext, ReactNode, useEffect, useMemo } from "react";
 
 // Define the shape of the context data
 interface ThemeContextType {
@@ -17,15 +17,41 @@ interface ThemeProviderProps {
 }
 
 const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(() => {
+    if (typeof window !== "undefined") {
+      const storedTheme = window.localStorage.getItem("theme");
+      return (
+        storedTheme ||
+        (window.matchMedia("(prefers-color-scheme: dark)").matches
+          ? "dark"
+          : "light")
+      );
+    }
+    return "light";
+  });
 
+  // Effect for applying the theme and persisting it
   useEffect(() => {
-    document.body.className = theme;
+    document.body.setAttribute("data-theme", theme);
+    window.localStorage.setItem("theme", theme);
   }, [theme]);
 
+  // Toggle theme and store preference
   const toggleTheme = () => {
-    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
+    setTheme((prevTheme) => {
+      const newTheme = prevTheme === "light" ? "dark" : "light";
+      window.localStorage.setItem("theme", newTheme);
+      return newTheme;
+    });
   };
+
+  const contextValue = useMemo(
+    () => ({
+      theme,
+      toggleTheme,
+    }),
+    [theme, toggleTheme],
+  );
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
