64de9b96f143dc4e452c50b45f7611516be521c1
Gee Chai <fc00004@gmail.com>
2024-03-11 10:13:47 +0900

    feat: dynamic fetching to firestore using TanStack/React Query

--- src/pages/lessons/index.tsx
+++ src/pages/lessons/index.tsx
@@ -1,19 +1,39 @@
 import Link from "next/link";
+import { useQuery } from "@tanstack/react-query";
+import {collection, getDocs} from "firebase/firestore";
+import { db } from "../../../firebaseConfig";
 import styles from "./AvailableYears.module.css";
+import Spinner from "@/components/Spinner/Spinner";
 
-const availableYears = ["2024", "2025"]; // Example years
+const fetchYears = async ()=>{
+  const yearsCollectionRef = collection(db, "years");
+  const yearsSnapshot = await getDocs(yearsCollectionRef);
+  return yearsSnapshot.docs.map(doc => doc.id);
+}
 
-const AvailableYears = () => (
-  <div className={styles.yearsContainer}>
-    <h1 className={styles.yearHeading}>Choose Your Learning Year</h1>
-    <div>
-      {availableYears.map((year) => (
-        <Link key={year} href={`/lessons/${year}`} passHref>
-          <button className={styles.yearButton}>{year}</button>
-        </Link>
-      ))}
+const AvailableYears = () => {
+  const {data: availableYears, isLoading, error} = useQuery({
+    queryKey: ['availableYears'],
+    queryFn: fetchYears
+  });
+  
+  if(isLoading) return <Spinner/>
+
+  if(error) return "An error has occurred: " + error.message
+  
+  return (
+    <div className={styles.yearsContainer}>
+      <h1 className={styles.yearHeading}>Choose Your Learning Year</h1>
+      <div>
+        {availableYears?.map((year) => (
+          <Link key={year} href={`/lessons/${year}`} passHref>
+            <button className={styles.yearButton}>{year}</button>
+          </Link>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+}
+
 
 export default AvailableYears;
