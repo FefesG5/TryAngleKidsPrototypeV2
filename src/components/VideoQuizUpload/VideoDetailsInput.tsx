dfbfbc9b9170f240b5b6bab9fba2c1508ee30006
Gee Chai <fc00004@gmail.com>
2024-03-22 09:14:05 +0900

    styles: simple CSS added to video details input

--- src/components/VideoQuizUpload/VideoDetailsInput.tsx
+++ src/components/VideoQuizUpload/VideoDetailsInput.tsx
@@ -1,4 +1,5 @@
 import { Video } from "@/types/quizTypes";
+import styles from "./VideoDetailsInput.module.css";
 
 const VideoDetailsInput: React.FC<{
   videoData: Video;
@@ -10,40 +11,44 @@ const VideoDetailsInput: React.FC<{
   };
 
   return (
-    <div>
+    <div className={styles.detailInputContainer}>
       <h2>Video Details</h2>
-      <label>
+      <label className={styles.detailInputLabel}>
         Year
         <input
           type="text"
           name="year"
+          className="detailInput"
           value={videoData.year}
           onChange={handleChange}
         />
       </label>
-      <label>
+      <label className={styles.detailInputLabel}>
         Lesson Number
         <input
           type="text"
           name="lessonNumber"
+          className="detailInput"
           value={videoData.lessonNumber}
           onChange={handleChange}
         />
       </label>
-      <label>
+      <label className={styles.detailInputLabel}>
         Video Source
         <input
           type="text"
           name="videoSrc"
+          className="detailInput"
           value={videoData.videoSrc}
           onChange={handleChange}
         />
       </label>
-      <label>
+      <label className={styles.detailInputLabel}>
         Category
         <input
           type="text"
           name="category"
+          className="detailInput"
           value={videoData.category}
           onChange={handleChange}
         />
