87c34a9ab7dcbc145e86e31c697e29b5235f1014
Gee Chai <fc00004@gmail.com>
2024-04-10 11:07:57 +0900

    feat: back to educator dashboard added for manageVideoQuizzes Component

--- src/pages/educator/video-quizzes/index.tsx
+++ src/pages/educator/video-quizzes/index.tsx
@@ -3,9 +3,8 @@ import Link from "next/link";
 import Spinner from "@/components/Spinner/Spinner";
 
 const ManageVideoQuizzes = () => {
-  // Assuming you want to list the years 2021-2024 for example
   const [years, setYears] = useState(["2024"]);
-  const [loading, setLoading] = useState(false); // Adjust based on actual loading state
+  const [loading, setLoading] = useState(false);
 
   if (loading) {
     return <Spinner />;
@@ -13,7 +12,10 @@ const ManageVideoQuizzes = () => {
 
   return (
     <>
+      <Link href="/educator/">Back to Dashboard</Link>
+
       <h1>Select a Year to Manage Video Quizzes</h1>
+
       <ul>
         {years.map((year) => (
           <li key={year}>
