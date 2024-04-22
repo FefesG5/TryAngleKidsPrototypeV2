dc11509efa91836ea24d6c2350c54db76a49d44e
Gee Chai <fc00004@gmail.com>
2024-04-22 10:41:44 +0900

    feat: fetchEducatorsForYear query from firestore created

--- src/pages/educators-lessons/index.tsx
+++ src/pages/educators-lessons/index.tsx
@@ -1,3 +1,12 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../../firebaseConfig";
+
+const fetchEducatorsForYear = async (year: string) => {
+  const educatorsRef = collection(db, "educators-years", year, "educators");
+  const querySnapshot = await getDocs(educatorsRef);
+  return querySnapshot.docs.map((doc) => doc.id);
+};
+
 const EducatorsLessons: React.FC = () => {
   return <>Educator Lessons Component</>;
 };
