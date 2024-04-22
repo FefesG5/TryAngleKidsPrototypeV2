ba3d75e8edd4fcac9098bf74a53a4f6010b8df1c
Gee Chai <fc00004@gmail.com>
2024-04-22 11:56:46 +0900

    styles: CSS module for index created

--- src/pages/educators-lessons/index.tsx
+++ src/pages/educators-lessons/index.tsx
@@ -1,45 +1,57 @@
-import { useRouter } from "next/router";
 import { useQuery } from "@tanstack/react-query";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../../firebaseConfig";
 import Spinner from "@/components/Spinner/Spinner";
+import Link from "next/link";
+import styles from "./EducatorsLessonsIndex.module.css";
 
-const fetchEducatorsForYear = async (year: string) => {
-  const educatorsRef = collection(db, "educators-years", year, "educators");
-  const querySnapshot = await getDocs(educatorsRef);
-  return querySnapshot.docs.map((doc) => doc.id);
+// Fetch the years available in the educators-years collection
+const fetchYears = async (): Promise<string[]> => {
+  const yearsRef = collection(db, "educators-years");
+  const querySnapshot = await getDocs(yearsRef);
+  return querySnapshot.docs.map((doc) => doc.id); // Assuming that years are document IDs
 };
 
-const EducatorsLessons: React.FC = () => {
-  const router = useRouter();
-  const year = router.query.year as string;
-
+const EducatorsLessonsIndex: React.FC = () => {
   const {
-    data: educators,
+    data: years,
     isLoading,
     error,
-  } = useQuery({
-    queryKey: ["educators", year],
-    queryFn: () => fetchEducatorsForYear(year),
-    enabled: !!year, // The query will only run if the year is not undefined
+  } = useQuery<string[], Error>({
+    queryKey: ["educators-years"],
+    queryFn: fetchYears,
   });
 
   if (isLoading) {
     return <Spinner />;
   }
 
-  if (error) return <div>An error has occurred: {error.message}</div>;
+  if (error) {
+    return (
+      <div>
+        An error has occurred:{" "}
+        {error instanceof Error ? error.message : "Unknown error"}
+      </div>
+    );
+  }
 
   return (
-    <div>
-      {year && <h1>Educators for {year}</h1>}
-      <ul>
-        {educators?.map((educatorId) => (
-          <li key={educatorId}>{educatorId}</li> // Replace with proper link or rendering logic
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Educator Lessons by Year</h1>
+      <ul className={styles.yearList}>
+        {years?.map((year) => (
+          <li key={year} className={styles.yearItem}>
+            <Link
+              href={`/educators-lessons/${year}`}
+              className={styles.yearLink}
+            >
+              Educators for {year}
+            </Link>
+          </li>
         ))}
       </ul>
     </div>
   );
 };
 
-export default EducatorsLessons;
+export default EducatorsLessonsIndex;
