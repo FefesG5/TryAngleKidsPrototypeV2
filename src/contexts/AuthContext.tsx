0da00b236f1a1f2a1f618f646ae4b0fb5fb2f623
Gee Chai <fc00004@gmail.com>
2024-04-23 13:41:10 +0900

    feat: AuthContext component added for authentication

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,45 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { app } from "../../firebaseConfig";
+
+interface AuthContextProps {
+  user: User | null;
+  loading: boolean;
+}
+
+const AuthContext = createContext<AuthContextProps>({
+  user: null,
+  loading: true,
+});
+
+export const useAuth = () => useContext(AuthContext);
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+  const auth = getAuth(app);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, [auth]);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
