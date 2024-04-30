eb8eebbaa16fd9d41ee5b2754dd416d421211835
Gee Chai <fc00004@gmail.com>
2024-04-30 11:17:11 +0900

    feat: LessonCard component to accept path to dynamically make correct href route

--- src/components/LessonCard/LessonCard.tsx
+++ src/components/LessonCard/LessonCard.tsx
@@ -5,12 +5,18 @@ interface LessonCardProps {
   id: string;
   category: string;
   year: string;
+  path: string;
 }
 
-const LessonCard: React.FC<LessonCardProps> = ({ id, category, year }) => {
+const LessonCard: React.FC<LessonCardProps> = ({
+  id,
+  category,
+  year,
+  path,
+}) => {
   return (
     <div className={styles.lessonCard}>
-      <Link href={`/lessons/${year}/${id}`} passHref>
+      <Link href={`/${path}/${year}/${id}`} passHref>
         <div className={styles.lessonCardLink}>
           <div className={styles.lessonCardBackground}></div>
           <div className={styles.lessonCardTitle}>
