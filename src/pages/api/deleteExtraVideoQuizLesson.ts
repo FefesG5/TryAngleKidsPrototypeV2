7fac324a567ce2af44b21a60be415939c669de2d
Gee Chai <fc00004@gmail.com>
2024-05-08 11:38:38 +0900

    feat: api deleteExtraVideoQuiz handler fn created

---
+++ src/pages/api/deleteExtraVideoQuizLesson.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../../firebaseConfig";
+import { doc, deleteDoc } from "firebase/firestore";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "DELETE") {
+    const { year, lessonNumber } = req.body;
+
+    if (!year || !lessonNumber) {
+      return res
+        .status(400)
+        .json({ message: "Year and lesson number are required" });
+    }
+
+    const lessonRef = doc(db, "extra", year, "lessons", lessonNumber);
+
+    try {
+      await deleteDoc(lessonRef);
+      res.status(200).json({ message: "Lesson deleted successfully" });
+    } catch (error) {
+      console.error("Error deleting document:", error);
+      res.status(500).json({ message: "Error deleting document", error });
+    }
+  } else {
+    res.setHeader("Allow", "DELETE");
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
