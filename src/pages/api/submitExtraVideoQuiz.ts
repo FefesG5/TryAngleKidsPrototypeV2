4ad9a1464457082e82943c4a44eff9d9d37a2f36
Gee Chai <fc00004@gmail.com>
2024-05-13 10:03:33 +0900

    feat: zod validation added to api/submitExtraVideoQuiz

--- src/pages/api/submitExtraVideoQuiz.ts
+++ src/pages/api/submitExtraVideoQuiz.ts
@@ -2,43 +2,47 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { db } from "../../../firebaseConfig";
 import { collection, doc, setDoc } from "firebase/firestore";
 import { Video, Question } from "@/types/quizTypes";
+import { z } from "zod";
+import { videoSchema } from "@/utils/schemas/zodSchemas";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method === "POST") {
-    const videoData: Video = req.body;
+  if (req.method !== "POST") {
+    res.setHeader("Allow", "POST");
+    res.status(405).json({ message: `Method ${req.method} Not Allowed` });
+    return;
+  }
 
-    if (!videoData.year || !videoData.lessonNumber) {
-      return res
-        .status(400)
-        .json({ message: "Year and lesson number must be set" });
-    }
+  try {
+    // Validate the incoming request body against the videoSchema
+    const parsedData: Video = videoSchema.parse(req.body); // This throws an error if data is invalid
 
     const lessonRef = doc(
-      collection(db, "extra", videoData.year, "lessons"),
-      videoData.lessonNumber,
+      collection(db, "extra", parsedData.year, "lessons"),
+      parsedData.lessonNumber,
     );
 
-    try {
-      await setDoc(lessonRef, {
-        ...videoData,
-        questions: videoData.questions.map((q: Question, index: number) => ({
-          ...q,
-          id: index + 1,
-        })),
-      });
+    await setDoc(lessonRef, {
+      ...parsedData,
+      questions: parsedData.questions.map((q: Question, index: number) => ({
+        ...q,
+        id: index + 1, // Ensure IDs are correctly assigned
+      })),
+    });
 
+    res
+      .status(200)
+      .json({ message: "Extra Video Quiz successfully submitted!" });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
       res
-        .status(200)
-        .json({ message: "Extra Video Quiz successfully submitted!" });
-    } catch (error) {
+        .status(400)
+        .json({ message: "Validation failed", errors: error.errors });
+    } else {
       console.error("Error writing document: ", error);
       res.status(500).json({ message: "Error writing document", error });
     }
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
