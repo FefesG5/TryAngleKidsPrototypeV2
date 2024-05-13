67368205df133da07f0e53b9bb1a37b8870c7009
Gee Chai <fc00004@gmail.com>
2024-05-13 09:58:17 +0900

    fix: zod validation added to api/submitVideoQuiz.ts file

--- src/pages/api/submitVideoQuiz.ts
+++ src/pages/api/submitVideoQuiz.ts
@@ -2,41 +2,49 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { db } from "../../../firebaseConfig";
 import { collection, doc, setDoc } from "firebase/firestore";
 import { Video, Question } from "@/types/quizTypes";
+import { z } from "zod";
+import { videoSchema } from "@/utils/schemas/zodSchemas";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method === "POST") {
-    const videoData: Video = req.body;
+  if (req.method !== "POST") {
+    res.setHeader("Allow", "POST");
+    res.status(405).json({ message: `Method ${req.method} Not Allowed` });
+    return;
+  }
 
-    if (!videoData.year || !videoData.lessonNumber) {
-      return res
-        .status(400)
-        .json({ message: "Year and lesson number must be set" });
-    }
+  try {
+    // Validate the incoming request body against the videoSchema
+    const parsedData: Video = videoSchema.parse(req.body);
+    const { year, lessonNumber } = parsedData;
 
     const lessonRef = doc(
-      collection(db, "years", videoData.year, "lessons"),
-      videoData.lessonNumber,
+      collection(db, "years", year, "lessons"),
+      lessonNumber,
     );
 
-    try {
-      await setDoc(lessonRef, {
-        ...videoData,
-        questions: videoData.questions.map((q: Question, index: number) => ({
-          ...q,
-          id: index + 1,
-        })),
-      });
+    // Add a database entry
+    await setDoc(lessonRef, {
+      ...parsedData,
+      questions: parsedData.questions.map((q: Question, index: number) => ({
+        ...q,
+        id: index + 1, // Optionally reassign IDs here
+      })),
+    });
 
-      res.status(200).json({ message: "Video Quiz successfully submitted!" });
-    } catch (error) {
+    res.status(200).json({ message: "Video Quiz successfully submitted!" });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      // Handle Zod errors specifically
+      res
+        .status(400)
+        .json({ message: "Validation failed", errors: error.errors });
+    } else {
+      // Handle other types of errors
       console.error("Error writing document: ", error);
       res.status(500).json({ message: "Error writing document", error });
     }
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
