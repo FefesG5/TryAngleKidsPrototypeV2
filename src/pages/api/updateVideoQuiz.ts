0fb6a889894bc06a938039768ca45844f5a54335
Gee Chai <fc00004@gmail.com>
2024-05-14 09:52:04 +0900

    fix: api/updateVideoQuiz validation with zod to match video format

--- src/pages/api/updateVideoQuiz.ts
+++ src/pages/api/updateVideoQuiz.ts
@@ -2,39 +2,42 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { db } from "../../../firebaseConfig";
 import { doc, updateDoc } from "firebase/firestore";
 import { Video } from "@/types/quizTypes";
+import { z } from "zod";
+import { videoSchema } from "@/utils/schemas/zodSchemas";
 
 export default async function updateHandler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method === "PATCH") {
-    const videoData: Video = req.body;
+  if (req.method !== "PATCH") {
+    res.setHeader("Allow", "PATCH");
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+    return;
+  }
 
-    if (!videoData.year || !videoData.lessonNumber) {
-      return res
+  try {
+    // Validate the incoming request body against the videoSchema
+    const parsedData: Video = videoSchema.parse(req.body);
+    const { year, lessonNumber } = parsedData;
+
+    const lessonRef = doc(db, "years", year, "lessons", lessonNumber);
+
+    // Update the database entry
+    await updateDoc(lessonRef, {
+      ...parsedData,
+    });
+
+    res.status(200).json({ message: "Video quiz successfully updated" });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      // Handle Zod errors specifically
+      res
         .status(400)
-        .json({ message: "Year and lesson number must be provided" });
-    }
-
-    const lessonRef = doc(
-      db,
-      "years",
-      videoData.year,
-      "lessons",
-      videoData.lessonNumber,
-    );
-
-    try {
-      await updateDoc(lessonRef, {
-        ...videoData,
-      });
-      res.status(200).json({ message: "Video quiz successfully updated" });
-    } catch (error) {
+        .json({ message: "Validation failed", errors: error.errors });
+    } else {
+      // Handle other types of errors
       console.error("Error updating document:", error);
       res.status(500).json({ message: "Error updating document" });
     }
-  } else {
-    res.setHeader("Allow", "PATCH");
-    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
