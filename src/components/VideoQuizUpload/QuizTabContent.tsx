303b1cca6f30d3fb061f3cec811027f1e4e0d132
Gee Chai <fc00004@gmail.com>
2024-05-26 10:12:06 +0900

    feat: quizTabContent display videoDetails or quiz questions details based on active tab to be integrated into other components

--- src/components/VideoQuizUpload/QuizTabContent.tsx
+++ src/components/VideoQuizUpload/QuizTabContent.tsx
@@ -2,7 +2,6 @@ import { Video, Question } from "@/types/quizTypes";
 import VideoDetailsInput from "./VideoDetailsInput";
 import QuestionDetailsInput from "./QuestionDetailsInput";
 import { defaultQuizQuestion } from "@/utils/videoQuizInitialState";
-import styles from "./QuizTabContent.module.css";
 
 interface QuizTabContentProps {
   activeTab: string;
@@ -18,25 +17,13 @@ const QuizTabContent: React.FC<QuizTabContentProps> = ({
   onQuestionChange,
 }) => {
   if (activeTab === "details") {
-    return (
-      <VideoDetailsInput
-        videoData={videoData}
-        onVideoDataChange={onVideoDataChange}
-      />
-    );
+    return <VideoDetailsInput videoData={videoData} onVideoDataChange={onVideoDataChange} />;
   }
 
-  const questionId = parseInt(activeTab.replace("questions", ""), 10);
-  const question =
-    videoData.questions.find((q) => q.id === questionId) ||
-    defaultQuizQuestion();
+  const questionId = parseInt(activeTab.replace("question-", ""), 10);
+  const question = videoData.questions.find((q) => q.id === questionId) || defaultQuizQuestion();
 
-  return (
-    <QuestionDetailsInput
-      questionData={question}
-      onQuestionChange={onQuestionChange}
-    />
-  );
+  return <QuestionDetailsInput questionData={question} onQuestionChange={onQuestionChange} />;
 };
 
 export default QuizTabContent;
