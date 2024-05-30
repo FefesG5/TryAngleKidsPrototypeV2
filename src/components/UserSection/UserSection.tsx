127e37843e4d6378096e6295219100f7b5fa71fa
Gee Chai <fc00004@gmail.com>
2024-05-30 09:57:16 +0900

    feat: UserSection component with signOut component created

---
+++ src/components/UserSection/UserSection.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { UserProfile } from "@/types/userProfileTypes";
+import SignOutButton from "../SignOutButton/SignOutButton";
+import styles from "./UserSection.module.css";
+
+interface UserSectionProps {
+  userProfile: UserProfile;
+  signOutUser: () => Promise<void>;
+}
+
+const UserSection = ({ userProfile, signOutUser }: UserSectionProps) => {
+  return (
+    <div className={styles.userSection}>
+      <Image
+        src={userProfile.photoURL ?? "/user_icon.svg"}
+        alt="user profile image"
+        width={50}
+        height={50}
+        className={styles.userPhoto}
+      />
+      <div className={styles.userInfo}>
+        <p>{userProfile.displayName}</p>
+        <p>{userProfile.email}</p>
+      </div>
+      <SignOutButton signOutUser={signOutUser} />
+    </div>
+  );
+};
+
+export default UserSection;
