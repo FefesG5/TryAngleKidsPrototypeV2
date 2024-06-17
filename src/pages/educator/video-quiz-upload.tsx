91a3a7a742864ca57cb72324ab7f051c98644e57
Gee Chai <fc00004@gmail.com>
2024-06-17 10:37:18 +0900

    feat: new container to hold title and link

--- src/pages/educator/video-quiz-upload.tsx
+++ src/pages/educator/video-quiz-upload.tsx
@@ -1,18 +1,21 @@
 import withAuth from "@/components/WithAuth/withAuth";
 import VideoQuizUploadForm from "@/components/VideoQuizUpload/VideoQuizUploadForm";
 import Link from "next/link";
+import styles from "./VideoQuizUploadForm.module.css";
 
 const VideoQuizUpload: React.FC = () => {
   // Component logic and JSX go here
 
   return (
-    <>
-      <h1>New Quiz Upload</h1>
-
-      <Link href="/educator">Back to Dashboard</Link>
-
+    <div className={styles.container}>
+      <div className={styles.headerSection}>
+        <h1 className={styles.title}>New Quiz Upload</h1>
+        <Link href="/educator" className={styles.backLink}>
+          Back to Dashboard
+        </Link>
+      </div>
       <VideoQuizUploadForm apiEndpoint="/api/submitVideoQuiz" />
-    </>
+    </div>
   );
 };
 
