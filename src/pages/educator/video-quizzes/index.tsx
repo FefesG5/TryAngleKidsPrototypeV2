f7aebe8e1e9d58b60bda0ac5de849fbb1f22fddd
Gee Chai <fc00004@gmail.com>
2024-07-01 10:29:46 +0900

    styles: stylesheet from extra-video-quizzes imported comment added for notice

--- src/pages/educator/video-quizzes/index.tsx
+++ src/pages/educator/video-quizzes/index.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../../../firebaseConfig";
 
+// Importing styles from the extra-video-quizzes module to maintain consistency
+import styles from "../extra-video-quizzes/ManageVideoQuizzes.module.css";
+
 interface ManageVideoQuizzesProps {
   years: string[];
 }
@@ -23,19 +26,24 @@ export async function getStaticProps() {
 
 const ManageVideoQuizzes = ({ years }: ManageVideoQuizzesProps) => {
   return (
-    <>
-      <Link href="/educator/">Back to Dashboard</Link>
-      <h1>Select a Year to Manage Video Quizzes</h1>
-      <ul>
+    <div className={styles.container}>
+      <Link href="/educator/" className={styles.backLink}>
+        Back to Dashboard
+      </Link>
+      <h1 className={styles.title}>Select a Year to Manage Video Quizzes</h1>
+      <ul className={styles.yearsList}>
         {years.map((year) => (
-          <li key={year}>
-            <Link href={`/educator/video-quizzes/${year}`}>
+          <li key={year} className={styles.yearItem}>
+            <Link
+              href={`/educator/video-quizzes/${year}`}
+              className={styles.manageLink}
+            >
               Manage {year} Quizzes
             </Link>
           </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 };
 
