86ffe8aec20ab180a6e8477fee71833b1a01bbc3
Gee Chai <fc00004@gmail.com>
2024-07-01 10:08:55 +0900

    feat: Upload New Extra Video Page have sample quiz template available to be copied

--- src/pages/educator/extra-video-quiz-upload.tsx
+++ src/pages/educator/extra-video-quiz-upload.tsx
@@ -1,19 +1,31 @@
-import withAuth from "@/components/WithAuth/withAuth";
-import VideoQuizUploadForm from "@/components/VideoQuizUpload/VideoQuizUploadForm";
+import { useState } from "react";
 import Link from "next/link";
+import VideoQuizUploadForm from "@/components/VideoQuizUpload/VideoQuizUploadForm";
+import withAuth from "@/components/WithAuth/withAuth";
+import SampleDataModal from "@/components/SampleDataModal/SampleDataModal";
+import styles from "./VideoQuizUploadForm.module.css"; // Reusing the same CSS file
 
-const VideoQuizUpload: React.FC = () => {
-  // Component logic and JSX go here
+const ExtraVideoQuizUpload: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
 
   return (
-    <>
-      <h1>New Extra Video Quiz Upload</h1>
-
-      <Link href="/educator">Back to Dashboard</Link>
-
+    <div className={styles.container}>
+      <div className={styles.headerSection}>
+        <h1 className={styles.title}>New Extra Video Quiz Upload</h1>
+        <Link href="/educator" className={styles.backLink}>
+          Back to Dashboard
+        </Link>
+      </div>
+      <button onClick={handleShowModal} className={styles.sampleButton}>
+        Copy Sample Data
+      </button>
+      <SampleDataModal show={showModal} onClose={handleCloseModal} />
       <VideoQuizUploadForm apiEndpoint="/api/submitExtraVideoQuiz" />
-    </>
+    </div>
   );
 };
 
-export default withAuth(VideoQuizUpload);
+export default withAuth(ExtraVideoQuizUpload);
